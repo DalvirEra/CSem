4ebd134a4f6c49096d4a8d726094c4b04a2739b5
Dalvir <ilya.hurs@gmail.com>
2022-11-29 22:06:03 +0300

    done

---
+++ Q3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = -a; i <= a; i++)
+{
+    Console.WriteLine(i);
+}
---
+++ Q4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+try{
+    Convert.ToInt32(a);
+    if (a.Length == 3) Console.WriteLine(a[2]);
+    else Console.WriteLine("Чото ни то");
+}   
+catch {Console.WriteLine("Чото ни то");}
+
+Console.WriteLine("Второй вариант, полегче ");
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a%10);
